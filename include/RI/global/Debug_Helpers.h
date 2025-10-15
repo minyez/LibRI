999ae56706d735d9a2ae90535b9a3da570120280
Minye Zhang <minyez.physchem@gmail.com>
2025-10-15 22:28:35 +0200

    fix: debug functions linkage

--- include/RI/global/Debug_Helpers.h
+++ include/RI/global/Debug_Helpers.h
@@ -7,6 +7,7 @@
 
 #include <string>
 #include <fstream>
+#include "Tensor.h"
 
 namespace RI
 {
@@ -14,7 +15,7 @@ namespace RI
 namespace Debug
 {
 
-extern std::string get_timestamp();
+std::string get_timestamp();
 
 class FileLogger
 {
@@ -33,7 +34,11 @@ public:
 /**
  * \brief Report virtual pages usage for memory check.
  */
-extern void report_virtual_pages(std::ostream &os);
+void report_virtual_pages(std::ostream &os);
+
+template<typename TA, typename TAC, typename Tdata>
+std::size_t count_map_total_size(const std::map<TA, std::map<TAC, Tensor<Tdata>>> &mm_tensor,
+                                 std::ostream &os, bool print_ac_keys = false);
 
 }
 
--- include/RI/global/Debug_Helpers.hpp
+++ include/RI/global/Debug_Helpers.hpp
@@ -14,13 +14,18 @@
 #include <malloc.h>
 #endif
 
+#if defined(__GLIBC__) && (__GLIBC__ * 1000 + __GLIBC_MINOR__ >= 2033)
+#define USE_MALLINFO2 1
+#endif
+
+
 namespace RI
 {
 
 namespace Debug
 {
 
-std::string get_timestamp()
+inline std::string get_timestamp()
 {
     auto now = std::chrono::system_clock::now();
     auto in_time_t = std::chrono::system_clock::to_time_t(now);
@@ -32,13 +37,13 @@ std::string get_timestamp()
     return ss.str();
 }
 
-void FileLogger::open(int myid)
+inline void FileLogger::open(int myid)
 {
     std::string fn = "debug_" + this->_prefix + "_myid_" + std::to_string(myid) + ".txt";
     this->_wf.open(fn.c_str());
 }
 
-void FileLogger::close()
+inline void FileLogger::close()
 {
     if (this->_wf.is_open())
     {
@@ -46,12 +51,16 @@ void FileLogger::close()
     }
 }
 
-void report_virtual_pages(std::ostream &os)
+inline void report_virtual_pages(std::ostream &os)
 {
 #ifndef __linux__
     os << "report_virtual_pages only available on Linux, skip" << std::endl;
 #else
+#ifdef USE_MALLINFO2
     struct mallinfo2 mi = mallinfo2();
+#else
+    struct mallinfo mi = mallinfo();
+#endif
     std::ifstream ifs("/proc/self/status");
     std::string line;
     std::string vmrss, vmdata;
@@ -65,6 +74,26 @@ void report_virtual_pages(std::ostream &os)
        << " MB, Mmap space: " << mi.hblkhd / (1024.0*1024)
        << " MB" << std::endl;
 #endif
+}
+
+template<typename TA, typename TAC, typename Tdata>
+std::size_t count_map_total_size(const std::map<TA, std::map<TAC, Tensor<Tdata>>> &mm_tensor,
+                                 std::ostream &os, bool print_a1c_keys)
+{
+    std::size_t total_size = 0;
+	for (const auto &data_a0: mm_tensor)
+	{
+	    os << "A0\t" << data_a0.first << "\twith (A1,C1) size\t" << data_a0.second.size() << std::endl;
+		for (const auto &data_a1c: data_a0.second)
+		{
+            total_size += data_a1c.second.get_shape_all();
+            if (print_a1c_keys)
+            {
+			    os << "    A1\t" << data_a1c.first.first << "\tsize\t" << data_a1c.second.get_shape_all() << std::endl;
+            }
+		}
+	}
+    return total_size;
 }
 
 }
